e62f714eadec13e3e6d0df9b68105905adc8df1a
Ali-Ulvi <ulvital@gmail.com>
2016-11-18 15:52:22 +0300

    wait

---
+++ src/test/java/com/AUTech/testSynch/PageLoadWaitExample.java
@@ -0,0 +1,34 @@
+package com.AUTech.testSynch;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Created by Ali Ulvi Talipoglu on 18.11.2016.
+ */
+
+public class PageLoadWaitExample {
+
+    static WebDriver driver;
+
+    @Test
+    public void testPageLoadSynchronization() throws Exception {
+        System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.11.1-win64\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.navigate().to("https://en.wikipedia.org/wiki/Main_Page");
+
+        By theLinkToNewPage = By.linkText("Log in");
+        //Element of the link
+        WebElement theLinkToNewPageElement = driver.findElement(theLinkToNewPage);
+        //Click the link
+        theLinkToNewPageElement.click();
+        WebDriverWait wait = new WebDriverWait(driver, 60, 100);
+        wait.until(ExpectedConditions.stalenessOf(theLinkToNewPageElement));
+        wait.until(ExpectedConditions.presenceOfElementLocated(theLinkToNewPage));
+    }
+}
